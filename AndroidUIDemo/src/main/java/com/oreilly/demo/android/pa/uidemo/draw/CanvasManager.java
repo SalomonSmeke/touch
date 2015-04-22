73e895b34297761a8db4d5b0ef74e65511b9b3fb
SalomonSmeke <ssmeke@luc.edu>
2015-04-22 09:51:17 -0500

    finished line

--- AndroidUIDemo/src/main/java/com/oreilly/demo/android/pa/uidemo/draw/CanvasManager.java
+++ AndroidUIDemo/src/main/java/com/oreilly/demo/android/pa/uidemo/draw/CanvasManager.java
@@ -6,18 +6,26 @@ package com.oreilly.demo.android.pa.uidemo.draw;
 public class CanvasManager implements Visitor<Void>{
 
     public Void onLine(Line l){
+
+
         return null;
     }
 
     public Void onSprite(Sprite sp){
+
+
         return null;
     }
 
     public Void onSquare(Square sq){
+
+
         return null;
     }
 
     public Void onText(Text t){
+
+
         return null;
     }
 
