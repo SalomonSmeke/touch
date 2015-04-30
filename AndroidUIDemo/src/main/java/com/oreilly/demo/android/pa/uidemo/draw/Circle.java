ae7af033034dd80bf6928544aaaa0220670a11c8
Thomas J. Fox <thomasjfox1@gmail.com>
2015-04-29 23:21:51 -0500

    more javadoc

--- AndroidUIDemo/src/main/java/com/oreilly/demo/android/pa/uidemo/draw/Circle.java
+++ AndroidUIDemo/src/main/java/com/oreilly/demo/android/pa/uidemo/draw/Circle.java
@@ -10,45 +10,94 @@ public class Circle implements DrawableObj{
         private int y;
         private int[] c;
 
-        public Circle(int radius, int x, int y, int[] c){
+        /** @param radius of the circle
+         * @param x coordinate
+         * @param  y coordinate
+         * @param c is color
+         * */
+         public Circle(int radius, int x, int y, int[] c){
             this.radius = radius;
             this.c = c;
             this.x = x;
             this.y = y;
         }
 
+        /**
+         *
+         * @param radius is the radius of the circle.
+         */
         public void setRadius (int radius){
             this.radius = radius;
         }
 
+        /**
+         *
+         * @param x x coordinate
+         */
         public void setX (int x){
             this.x = x;
         }
 
+        /**
+         *
+         * @param y y coordinate
+         */
         public void setY (int y){
             this.y = y;
         }
 
+        /**
+         *
+         * @param c color
+         */
         public void setC (int[] c) { this.c = c; }
 
+        /**
+         *
+         * @return radius
+         */
         public int getRadius (){
             return radius;
         }
 
+        /**
+         *
+         * @return y coordinate
+         */
         public int getY (){
             return y;
         }
 
+        /**
+         *
+         * @return x coordinate
+         */
         public int getX (){
             return x;
         }
 
+        /**
+         *
+         * @return color
+         */
         public int[] getC () { return c; }
 
+        /**
+         *
+         * @param x x coordinate
+         * @param y y coordinate
+         * @return false
+         */
         public boolean hit(int x, int y){
             return false;
         }
 
+        /**
+         *
+         * @param v visitor
+         * @param <Result> object type
+         * @return circle visitor
+         */
         public <Result> Result accept(final Visitor<Result> v) {
             return v.onCircle(this);
         }}
--- AndroidUIDemo/src/main/java/com/oreilly/demo/android/pa/uidemo/draw/CircleF.java
+++ AndroidUIDemo/src/main/java/com/oreilly/demo/android/pa/uidemo/draw/CircleF.java
@@ -13,6 +13,13 @@ public class CircleF implements DrawableObj {
     private int y;
     private int[] c;
 
+    /**
+     *
+     * @param radius circle radius
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param c color
+     */
     public CircleF(int radius, int x, int y, int[] c){
         this.radius = radius;
         this.c = c;
@@ -20,38 +27,82 @@ public class CircleF implements DrawableObj {
         this.y = y;
     }
 
+    /**
+     *
+     * @param radius circle radius
+     */
     public void setRadius (int radius){
         this.radius = radius;
     }
 
+    /**
+     *
+     * @param x x coordinate
+     */
     public void setX (int x){
         this.x = x;
     }
 
+    /**
+     *
+     * @param y y coordinate
+     */
     public void setY (int y){
         this.y = y;
     }
 
+    /**
+     *
+     * @param c color
+     */
     public void setC (int[] c) { this.c = c; }
 
+    /**
+     *
+     * @return radius
+     */
     public int getRadius (){
         return radius;
     }
 
+    /**
+     *
+     * @return y coordinate
+     */
     public int getY (){
         return y;
     }
 
+    /**
+     *
+     * @return x coordinate
+     */
     public int getX (){
         return x;
     }
 
+    /**
+     *
+     * @return color
+     */
     public int[] getC () { return c; }
 
+    /**
+     *
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return false
+     */
     public boolean hit(int x, int y){
         return false;
     }
 
+    /**
+     *
+     * @param v visitor
+     * @param <Result> object type
+     * @return visitor object
+     */
     public <Result> Result accept(final Visitor<Result> v) {
         return v.onCircleF(this);
     }
